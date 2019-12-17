b488292e000a3f1686654bd99cbd4525490644e2
Menoetius <1683951561@qq.com>
2019-12-17 15:03:17 +0800

    spring boot day 1

---
+++ src/main/java/com/atguigu/HelloWorkdMainApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//说明这是一个spring boot工程
+public class HelloWorkdMainApplication {
+    public static void main(String[] args) {
+
+        //启动SPRING应用
+        SpringApplication.run(HelloWorkdMainApplication.class,args);
+    }
+}
